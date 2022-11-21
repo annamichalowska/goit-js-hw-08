4e28da2f3c8f2faeace864278e718ed2fd7692c8
Anna <michalowska.aneczka@gmail.com>
2022-11-21 21:35:40 +0100

    copy_src

---
+++ src/js/01-gallery.js
@@ -0,0 +1,29 @@
+// Add imports above this line
+import { galleryItems } from "./gallery-items";
+// Change code below this line
+//import SimpleLightbox from "simplelightbox";
+//import "simplelightbox/dist/simple-lightbox.min.css";
+
+console.log(galleryItems);
+
+const placeOfGallery = document.querySelector(".gallery");
+const mainGallery = galleryItems;
+
+const createGallery = mainGallery
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+placeOfGallery.insertAdjacentHTML("beforeend", createGallery);
+
+console.log(galleryItems);
